b3bf913c9001154cefa177201db5795539f0d81e
Shilpa Jesadiya <shilpa.jesadiya@mindinventory.com>
2023-02-22 18:51:48 +0530

    - Updated DB connection

--- app.js
+++ app.js
@@ -5,26 +5,10 @@ const blogRouter = require("./routes/BlogRoutes");
 
 const MONGODB_URI= "mongodb+srv://shilpa:mind%40123@node-crud.9lptvoh.mongodb.net/?retryWrites=true&w=majority"
 
-/*mongoose.connect(
-    process.env.MONGODB_URI || URL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
-    (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Connected to MongoDB");
-        }
-    }
-);*/
-
-
 const connectDB = async () => {
     console.log(process.env.MONGODB_URI, 'AAAAAAAAAAAAAAAAA')
     try {
-        const conn = await mongoose.connect(MONGODB_URI || "mongodb://localhost/CRUD");
+        const conn = await mongoose.connect(MONGODB_URI );
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
